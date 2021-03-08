80991a11aa9c35488c4d2581086cda7ca8d36600
cslunetta <cslunetta@outlook.com>
2021-03-08 17:28:46 -0600

    added team member for heist

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace PlanHeist
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Plan Your Heist!");
+
+      Console.Write("Enter a team members name: ");
+      string name = Console.ReadLine();
+      Console.Write("What Is there skill level? (1-10): ");
+      string skill = Console.ReadLine();
+      int skillLevel = int.Parse(skill);
+      Console.Write("How couragous are they? (0.0-2.0): ");
+      string courage = Console.ReadLine();
+      decimal courageFactor = decimal.Parse(courage);
+
+      TeamMember member = new TeamMember(name, skillLevel, courageFactor);
+
+      Console.WriteLine($@"
+Member: {member.Name} 
+Skill Level: {member.Skill} 
+Courage Factor: {member.Courage}");
     }
   }
 }
